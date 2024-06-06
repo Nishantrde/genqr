81e90ad79cf98bf7666ede6f1b9fb94b26c6f3ef
Nishant <nishant2024rde@gmail.com>
2024-06-05 23:37:34 -0700

    Initial Commit

--- mainapp/qr_gen.py
+++ mainapp/qr_gen.py
@@ -4,26 +4,43 @@ from tempfile import NamedTemporaryFile
 from .models import *
 import os
 import uuid
+from PIL import Image, ImageDraw, ImageFilter
 
 
 def generate_qr_code(data):
     key = str(uuid.uuid4())
     print(key)
     qr_data = "https://genqr-ten.vercel.app/visit/"+key
-    qr = qrcode.QRCode(
+    qr = qrcode.QRCode(#qrcode
         version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
-        border=4,   
+        border=4,
     )
     qr.add_data(qr_data)
     qr.make(fit=True)
 
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color=(0, 255, 0), back_color=(0, 0, 0)).convert("RGBA")
+
+    overlay = Image.new("RGBA", img.size)
+    draw = ImageDraw.Draw(overlay)
+
+    center_x, center_y = img.width // 2, img.height // 2
+    radius = min(img.width, img.height) // 2
+    
+    for r in range(radius,0,-1):
+        alpha = int(255 * (0.2 - r/radius))
+        color = (24, 21, 245, alpha)
+        draw.ellipse(
+            (center_x - r, center_y - r, center_x + r, center_y + r),
+            fill =  color,
+            outline = None
+        )
+    img = Image.alpha_composite(img, overlay)
 
     # Save QR code image to a temporary file
     temp_file = NamedTemporaryFile(delete=False)
-    img.save(temp_file.name)
+    img.save(temp_file.name, format='PNG')
 
     # Upload image to Cloudinary
     cloudinary_response = cloudinary.uploader.upload(temp_file.name)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from .models import *
 from .qr_gen import generate_qr_code
 
 def index(request):
-    qr_img = None
     if request.method == "POST":
         site_add = request.POST.get("site")
         print(site_add)
         qr_img = generate_qr_code(site_add)
-    return render(request, "index.html", {"photo":qr_img})
+        # Save the QR code image or its identifier in session or database if you need to persist it
+        request.session["qr_image"] = qr_img  # Example usage with session
+        return HttpResponseRedirect(request.path_info)  # Redirect to the same page after POST
+    else:
+        qr_img = request.session.pop("qr_image", None)  # Retrieve QR image from session
+        print("here")
+    return render(request, "index.html", {"photo": qr_img})
 
 def visit(request, uuid):
     print(uuid)
     site = Qrcodes.objects.get(uuid=uuid)
     print(site.site)
     return redirect(site.site)
-
-
