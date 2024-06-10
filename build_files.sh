1eebc38216efa3bb19c8439fd0a7dd508b0faca6
Nishant <nishant2024rde@gmail.com>
2024-06-09 19:55:13 -0700

    Initial Commit

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,10 @@
 # Ensure script stops on error
 set -e
 
+# Install Nginx
+apt-get update
+apt-get install -y nginx
+
 # Create and activate a virtual environment
 python3 -m venv venv
 source venv/bin/activate
@@ -12,6 +16,9 @@ pip install --upgrade pip
 
 # Install Python packages from requirements.txt
 pip install -r requirements.txt
+
+# Start Nginx
+service nginx start
 
 # Add any other necessary build steps below
 # For example, if you need to run database migrations or collect static files for Django:
