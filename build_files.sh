5f6546add518a5149b6d9689128b301efc1e26f0
Nishant <nishant2024rde@gmail.com>
2024-06-09 19:49:40 -0700

    Initial Commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Update package lists
+sudo yum update -y
+
+# Install required dependencies
+sudo yum install -y python3-pip python3-devel gcc
+
+# Install Python packages
 pip3 install -r requirements.txt
-cat /etc/*release
